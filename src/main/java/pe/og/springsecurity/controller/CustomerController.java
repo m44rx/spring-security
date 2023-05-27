c3750a65b3254b7c57f09fe245793861d45cf110
Eddie Mallma Alvarez <emallmaa@proovedor.intercorp.com.pe>
2023-05-27 07:05:20 -0500

    init commit

---
+++ src/main/java/pe/og/springsecurity/controller/CustomerController.java
@@ -0,0 +1,22 @@
+package pe.og.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1")
+public class CustomerController {
+    
+    @GetMapping("/index")
+    public String index(){
+        return "hello world";
+    }
+
+    @GetMapping("/index2")
+    public String index2(){
+        return "hello world not secure";
+    }
+
+
+}
