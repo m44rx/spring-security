eaf2da95f1c43efaf7bb4479a8e7eff453be5df4
Eddie Mallma Alvarez <emallmaa@proovedor.intercorp.com.pe>
2023-05-28 21:00:39 -0500

    add 2 different properties for customize security to access a endpoints

--- src/main/java/pe/og/springsecurity/config/SecurityConfig.java
+++ src/main/java/pe/og/springsecurity/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -28,41 +30,57 @@ public class SecurityConfig {
                     login.permitAll();
                 })
                 .sessionManagement(management -> {
+                    //  Ventaja de guardar sesion de usuarios, guarda cierta informacion para evitar autentificar repeditadamente
                     management.sessionCreationPolicy(SessionCreationPolicy.ALWAYS); // ALWAYS - IF_REQUIRED - NEVER -STATELESS
-
+                    management.invalidSessionUrl("/login");
+                    // .sessionManagement().maximumSessions(1).and().invalidSessionUrl("/expired"); **examplee
+                    management.maximumSessions(1).expiredUrl("/login");
+                    //  Resuelve vulnerabilidades
+                    management.sessionFixation()
+                            //  Con el migrationSession springSecurity genera nuevo id de sesión para autentificar
+                            .migrateSession();
                 })
                 .build();
-    }
+    }   
 
+
+    //  Obtenerlos datos de la sesión
+    @Bean
+    public SessionRegistry sessionRegistry(){
+        return new SessionRegistryImpl();
+    }
+    
+    
     public AuthenticationSuccessHandler successHandler() {
         return ((request, response, authentication) -> {
-            response.sendRedirect("/v1/index");
+            response.sendRedirect("/v1/session");
         });
     }
 
-    /*
-     * -> Configuration One <-
-     * 
-     * @Bean
-     * public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws
-     * Exception{
-     * return httpSecurity
-     * // Cross-Site Request Forgery - csrf - formulario a través del navegador, por
-     * defecto activo que resuleve vulnerabilidades - .csrf().disable para
-     * desactivar
-     * .authorizeHttpRequests()
-     * // No necesitara autorizacion en requestMatchers + permitAll
-     * // Solicitudes que apunten especificamnt a este path, se les permite a todos
-     * .requestMatchers("/v1/index2").permitAll()
-     * // Solicitudes a otros path deben ser autenticadas
-     * .anyRequest().authenticated()
-     * .and()
-     * // Permitir a todos acceder al formulario
-     * .formLogin(login -> login.permitAll())
-     * .build();
-     * 
-     * return httpSecurity.build();
-     * }
-     */
+        // -> Configuration One <-
+    
+    // @Bean
+    // public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws
+    // Exception{
+    // return httpSecurity
+    //         // Cross-Site Request Forgery - csrf - formulario a través del navegador, por
+    //         // defecto activo que resuleve vulnerabilidades - .csrf().disable para desactivar
+    //         .authorizeHttpRequests()
+    //         // No necesitara autorizacion en requestMatchers + permitAll
+    //         // Solicitudes que apunten especificamnt a este path, se les permite a todos
+    //         .requestMatchers("/v1/index2").permitAll()
+    //         // Solicitudes a otros path deben ser autenticadas
+    //         .anyRequest().authenticated()
+    //         .and()
+    //         // Permitir a todos acceder al formulario
+    //         .formLogin(login -> login.permitAll())
+    //         //  Abre la posibilildad a enviar el user y pass por medio de los headers
+    //         .httpBasic()
+    //         .and()
+    //         .build();
+    
+    // // return httpSecurity.build();
+    // }
+    
 
 }
--- src/main/java/pe/og/springsecurity/controller/CustomerController.java
+++ src/main/java/pe/og/springsecurity/controller/CustomerController.java
@@ -1,12 +1,26 @@
 package pe.og.springsecurity.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("v1")
 public class CustomerController {
+
+
+    private final SessionRegistry sessionRegistry;
     
     @GetMapping("/index")
     public String index(){
@@ -18,5 +32,37 @@ public class CustomerController {
         return "hello world not secure";
     }
 
+
+    @GetMapping("/session")
+    public ResponseEntity<?> getDeatilsSession(){
+
+        String sessionId = "";
+        User userObject = null;
+
+        List<Object> sessions = sessionRegistry.getAllPrincipals();
+        
+        for(Object session : sessions){
+            if(session instanceof User){
+                userObject = (User) session;
+            }
+
+            List<SessionInformation> sessionInformations = sessionRegistry.getAllSessions(session, false);
+
+            for(SessionInformation sessionInformation: sessionInformations){
+                sessionId = sessionInformation.getSessionId();
+            }
+            
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("response","Hellow world");
+        response.put("sessionId", sessionId);
+        response.put("sessionUser",userObject);
+
+        return  ResponseEntity.ok(response);
+    }
+    
+    
+    
 
 }
